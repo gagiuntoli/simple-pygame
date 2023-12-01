4a3dcfe2599b4f872c214dcf4887abc10c8355a8
Guido Giuntoli <gagiuntoli@gmail.com>
2023-12-01 12:10:16 +0100

    Insert points in quad-tree

--- quad_tree.py
+++ quad_tree.py
@@ -1,9 +1,11 @@
 
 class QuadTree:
-    def __init__(self, rectangle: list[float], leaf_size: float):
+    def __init__(self, rectangle: list[float], leaf_size: float, extended_distance: float):
 
         self.leaves = []
         self.points = []
+        self.extended_distance = extended_distance
+        self.rectangle = rectangle
 
         [xmin, xmax, ymin, ymax] = rectangle
 
@@ -12,10 +14,10 @@ class QuadTree:
         if max(width, height) > leaf_size:
             width /= 2
             height /= 2
-            self.leaves.append(QuadTree([xmin, xmax-width, ymin, ymax-height], leaf_size))
-            self.leaves.append(QuadTree([xmin+width, xmax, ymin, ymax-height], leaf_size))
-            self.leaves.append(QuadTree([xmin, xmax-width, ymin+height, ymax], leaf_size))
-            self.leaves.append(QuadTree([xmin+width, xmax, ymin+height, ymax], leaf_size))
+            self.leaves.append(QuadTree([xmin, xmax-width, ymin, ymax-height], leaf_size, extended_distance))
+            self.leaves.append(QuadTree([xmin+width, xmax, ymin, ymax-height], leaf_size, extended_distance))
+            self.leaves.append(QuadTree([xmin, xmax-width, ymin+height, ymax], leaf_size, extended_distance))
+            self.leaves.append(QuadTree([xmin+width, xmax, ymin+height, ymax], leaf_size, extended_distance))
 
     def height(self):
         height = 0
@@ -24,4 +26,34 @@ class QuadTree:
             current = current.leaves[0]
             height += 1
         return height
+
+
+    def is_inside_rectangle(self, rectangle, point):
+        [xmin, xmax, ymin, ymax] = rectangle
+        (x, y) = point
+
+        return xmin <= x and x <= xmax and ymin <= y and y <= ymax
+
+
+    def insert_point(self, point):
+        if self.leaves == []:
+            self.points.append(point)
+            return
+
+        [xmin, xmax, ymin, ymax] = self.rectangle
+        dd = self.extended_distance
+        width, height = xmax-xmin, ymax-ymin
+        width /= 2
+        height /= 2
+
+        if self.is_inside_rectangle([xmin, xmin+width+dd, ymin, ymin+height+dd], point):
+            self.leaves[0].insert_point(point)
+        if self.is_inside_rectangle([xmin+width-dd, xmax, ymin, ymin+height+dd], point):
+            self.leaves[1].insert_point(point)
+        if self.is_inside_rectangle([xmin, xmin+width+dd, ymin+height-dd, ymax], point):
+            self.leaves[2].insert_point(point)
+        if self.is_inside_rectangle([xmin+width-dd, xmax, ymin+height-dd, ymax], point):
+            self.leaves[3].insert_point(point)
+
+
 
--- quad_tree_test.py
+++ quad_tree_test.py
@@ -2,18 +2,83 @@ from quad_tree import QuadTree
 
 def test_check_height():
 
-    quad_tree = QuadTree([0,1,0,1], 1.1)
+    dist = 0.0
+    quad_tree = QuadTree([0,1,0,1], 1.1, dist)
     assert quad_tree.height() == 0
 
-    quad_tree = QuadTree([0,1,0,1], 0.9)
+    quad_tree = QuadTree([0,1,0,1], 0.9, dist)
     assert quad_tree.height() == 1
 
-    quad_tree = QuadTree([0,1,0,1], 0.49)
+    quad_tree = QuadTree([0,1,0,1], 0.49, dist)
     assert quad_tree.height() == 2
 
-    quad_tree = QuadTree([0,1,0,1], 0.24)
+    quad_tree = QuadTree([0,1,0,1], 0.24, dist)
     assert quad_tree.height() == 3
 
-    quad_tree = QuadTree([0,1,0,1], 0.12)
+    quad_tree = QuadTree([0,1,0,1], 0.12, dist)
     assert quad_tree.height() == 4
+
+
+def test_insert_point():
+    dist = 0.1
+    quad_tree = QuadTree([0,1,0,1], 0.9, dist)
+    quad_tree.insert_point((0.25, 0.25))
+    assert quad_tree.leaves[0].points == [(0.25, 0.25)]
+    assert quad_tree.leaves[1].points == []
+    assert quad_tree.leaves[2].points == []
+    assert quad_tree.leaves[3].points == []
+
+    dist = 0.1
+    quad_tree = QuadTree([0,1,0,1], 0.9, dist)
+    quad_tree.insert_point((0.45, 0.45))
+    assert quad_tree.leaves[0].points == [(0.45, 0.45)]
+    assert quad_tree.leaves[1].points == [(0.45, 0.45)]
+    assert quad_tree.leaves[2].points == [(0.45, 0.45)]
+    assert quad_tree.leaves[3].points == [(0.45, 0.45)]
+
+    dist = 0.0
+    quad_tree = QuadTree([0,1,0,1], 0.9, 0.0)
+    quad_tree.insert_point((0.45, 0.45))
+    assert quad_tree.leaves[0].points == [(0.45, 0.45)]
+    assert quad_tree.leaves[1].points == []
+    assert quad_tree.leaves[2].points == []
+    assert quad_tree.leaves[3].points == []
+
+    dist = 0.1
+    quad_tree = QuadTree([0,1,0,1], 0.9, dist)
+    point = (0.55, 0.55)
+    quad_tree.insert_point(point)
+    assert quad_tree.leaves[0].points == [point]
+    assert quad_tree.leaves[1].points == [point]
+    assert quad_tree.leaves[2].points == [point]
+    assert quad_tree.leaves[3].points == [point]
+
+    dist = 0.1
+    quad_tree = QuadTree([0,1,0,1], 0.9, dist)
+    point = (0.45, 0.55)
+    quad_tree.insert_point(point)
+    assert quad_tree.leaves[0].points == [point]
+    assert quad_tree.leaves[1].points == [point]
+    assert quad_tree.leaves[2].points == [point]
+    assert quad_tree.leaves[3].points == [point]
+
+    dist = 0.1
+    quad_tree = QuadTree([0,1,0,1], 0.9, dist)
+    point = (0.55, 0.45)
+    quad_tree.insert_point(point)
+    assert quad_tree.leaves[0].points == [point]
+    assert quad_tree.leaves[1].points == [point]
+    assert quad_tree.leaves[2].points == [point]
+    assert quad_tree.leaves[3].points == [point]
+
+    dist = 0.1
+    quad_tree = QuadTree([0,1,0,1], 0.9, dist)
+    p1 = (0.55, 0.45)
+    p2 = (0.25, 0.25)
+    quad_tree.insert_point(p1)
+    quad_tree.insert_point(p2)
+    assert quad_tree.leaves[0].points == [p1, p2]
+    assert quad_tree.leaves[1].points == [p1]
+    assert quad_tree.leaves[2].points == [p1]
+    assert quad_tree.leaves[3].points == [p1]
 
